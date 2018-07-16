4ede80096764a429ca510e96c65c09537594144f
Jake Wells <jakedwells@outlook.com>
2018-07-16 13:51:48 +0000

    change background colour depending on guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,31 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-secret_number = rand(101)
+SECRET_NUMBER = rand(101)
+$background_color = "white"
 
 get '/' do 
-    "The SECRET NUMBER is #{secret_number}"
+    guess = params["guess"]
+    cheat = params["cheat"]
+    message = check_guess(guess)
+    erb :index, :locals => {:message => message, :background_color => $background_color, :cheat => cheat, :secret_number => SECRET_NUMBER}
+end
+
+def check_guess(guess)
+        if params["guess"].to_i > SECRET_NUMBER && params["guess"].to_i <= SECRET_NUMBER + 5
+            $background_color = "pink"
+            "Too High"
+        elsif params["guess"].to_i < SECRET_NUMBER && params["guess"].to_i >= SECRET_NUMBER - 5
+             $background_color = "pink"
+            "Too Low"
+        elsif params["guess"].to_i < SECRET_NUMBER
+            $background_color = "red"
+            "Far too low"
+        elsif params["guess"].to_i >  SECRET_NUMBER
+            $background_color = "red"
+            "Far too high"
+        else
+            $background_color = "green"
+            "You got it right! The Secret Number is #{SECRET_NUMBER}"
+        end
 end
